68c292933ff8441288df00df6f7f6927d0dad49a
Basement Science <BaseSc@protonmail.com>
2021-02-24 21:32:36 +0100

    Added NoColor option, refactoring

--- TimeStamp/TimeStamp/Program.cs
+++ TimeStamp/TimeStamp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -14,10 +15,12 @@ namespace TimeStamp {
             bool TimeStampNewlines = false;
             bool UseLocalTimeZone = false;
             bool FullColor = false;
+            bool NoColor = false;
 
             string[] OptionStrings_TimeStampNewlines = { "/n", "-n", "--stampNewLines" };
             string[] OptionStrings_LocalTimeZone = { "/l", "-l", "--localTime" };
             string[] OptionStrings_FullColor = { "/c", "-c", "--fullColor" };
+            string[] OptionStrings_NoColor = { "/nc", "-nc", "--noColor" };
 
             // handle parameters
             foreach ( string arg in args ) {
@@ -28,14 +31,84 @@ namespace TimeStamp {
                     UseLocalTimeZone = true;
                 } else if ( ProcessCMDoption_bool(OptionStrings_FullColor, argUpper) ) {
                     FullColor = true;
+                } else if ( ProcessCMDoption_bool(OptionStrings_NoColor, argUpper) ) {
+                    NoColor = true;
                 } else {
                     printHelpScreen();
+                    CleanUp();
                     return;
                 }
             }
 
+            //string test1 = "test1\n";
+            //string test2 = "test2";
+            //Console.Write(test1.Pastel("#FF7800") + " extra");
+            //Console.WriteLine(test2.Pastel("#FF3500") + " extra");
+            //Console.WriteLine("not very colored?");
+
+
+            // handle the input
+            if ( Console.IsInputRedirected ) {
+                // Program was called with a pipe input
+                string s;
+                while ( (s = Console.ReadLine()) != null ) {
+                    if ( TimeStampNewlines || s.Length != 0 ) {
+                        PrintTimeStamp(" - ");
+                    }
+                    Console.WriteLine(s);
+                }
+            } else {
+                // Program was called directly
+                PrintTimeStamp("\n");
+            }
+
+            /* Prints the Timestamp in UTC or local time, plus another string after it. */
+            void PrintTimeStamp(string postFix) {
+                DateTime time = UseLocalTimeZone ? DateTime.Now : DateTime.UtcNow;
+                string[] stamp = time.ToString("u", System.Globalization.CultureInfo.InvariantCulture).TrimEnd('Z').Split(' ', 2);
+                stamp[0] += " ";
+                string end = UseLocalTimeZone ? "" : (FullColor ? "Z".Pastel("#808080") : "Z");
+                StringBuilder builder;
+                string output;
+
+                Console.Write('[');
+                if ( FullColor ) {
+                    builder = new StringBuilder(stamp[0].Pastel("#FF7800")).Append(stamp[1].Pastel("#FF3500")).Append(end);
+                    output = builder.ToString().PastelBg(Color.Black);
+                    Console.Write(output);
+                } else {
+                    builder = new StringBuilder(stamp[0]).Append(stamp[1]).Append(end);
+                    output = builder.ToString();
+                    if ( ! NoColor ) {
+                        // Use oldschool color
+                        ConsoleColor oldBackground = Console.BackgroundColor;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        ConsoleColor oldForeground = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(output);
+                        Console.ForegroundColor = oldForeground;
+                        Console.BackgroundColor = oldBackground;
+                    } else {
+                        // No color
+                        Console.Write(output);
+                    }
+                }
+                Console.Write(']' + postFix);
+            }
+
+            /* Compares argLower (a lowercase string) to all elements of the Array OptionStrings. 
+             * If a match is found, returns true, else returns false.
+             */
+            static bool ProcessCMDoption_bool(string[] OptionStrings, string argLower) {
+                foreach ( string possibleArg in OptionStrings ) {
+                    if ( argLower == possibleArg.ToUpper(CultureInfo.InvariantCulture) ) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
             void printHelpScreen() {
-                // print help
                 string ProgramName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
                 Console.WriteLine(ProgramName + " utility - Adds a Timestamp to the beginning of each line from the pipe |");
 
@@ -84,53 +157,6 @@ namespace TimeStamp {
                 p.Close();
             }
 
-            // handle the input
-            if ( Console.IsInputRedirected ) {
-                // Program was called with a pipe input
-                string s;
-                while ( (s = Console.ReadLine()) != null ) {
-                    if ( TimeStampNewlines || s.Length != 0 ) {
-                        PrintTimeStamp(" - ");
-                    }
-                    Console.WriteLine(s);
-                }
-            } else {
-                // Program was called directly
-                PrintTimeStamp("\n");
-            }
-
-            /* Prints the Timestamp in UTC or local time, plus another string after it.
-             * Does not add a newline.
-             */
-            void PrintTimeStamp(string postFix) {
-                string[] temp;
-                if ( UseLocalTimeZone ) {
-                    // "u" option actually seems to ignore the IFormatProvider you supply. It is here anyway to make a warning go away.
-                    temp = DateTime.Now.ToString("u", System.Globalization.CultureInfo.InvariantCulture).TrimEnd('Z').Split(' ', 2);
-                } else {
-                    // see above
-                    temp = DateTime.UtcNow.ToString("u", System.Globalization.CultureInfo.InvariantCulture).TrimEnd('Z').Split(' ', 2);
-                    temp[1] += FullColor ? "Z".Pastel("#808080") : "Z";
-                }
-                Console.Write('[');
-
-                ConsoleColor oldBackground = Console.BackgroundColor;
-                Console.BackgroundColor = ConsoleColor.Black;
-                ConsoleColor oldForeground = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Blue;
-                if ( FullColor ) {
-                    Console.Write(temp[0].Pastel("#FF7800") + ' ');
-                    Console.Write(temp[1].Pastel("#FF3500"));
-                } else {
-                    Console.Write(temp[0] + ' ');
-                    Console.Write(temp[1]);
-                }
-                Console.ForegroundColor = oldForeground;
-                Console.BackgroundColor = oldBackground;
-
-                Console.Write(']' + postFix);
-            }
-
             /* Concats the strings from an array EXCEPT the first one, separated by a ", "
              * Returns a StringBuilder to allow further efficient Concatenations
              */
@@ -144,22 +170,16 @@ namespace TimeStamp {
                 }
                 return optionHelp;
             }
+            CleanUp();
+        } // END MAIN()
+
+        private static void CleanUp() {
+            //Console.ResetColor();
         }
 
         protected static void ExitHandler(object sender, ConsoleCancelEventArgs e) {
+            CleanUp();
             e.Cancel = true;
-        }
-
-        /* Compares argLower (a lowercase string) to all elements of the Array OptionStrings. 
-         * If a match is found, returns true, else returns false.
-         */
-        private static bool ProcessCMDoption_bool(string[] OptionStrings, string argLower) {
-            foreach ( string possibleArg in OptionStrings ) {
-                if ( argLower == possibleArg.ToUpper(CultureInfo.InvariantCulture) ) {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
